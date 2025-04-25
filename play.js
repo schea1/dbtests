aeea00ce73a7f521e57c429e3f304ba46dbc6d2c
Sean Chea <sean@rstcore.com>
2025-04-24 20:44:51 -0700

    first release

---
+++ play.js
@@ -0,0 +1,6 @@
+const myProdObject = {
+  name: "My Product",
+    price: 100.0,
+    description: "This is a test product"
+};
+const myProdArray = [1, "My Product", "This is a test product", 100.0];
